a061ead840471650a085de30532e0002dd42fa1e
Ateeth <58625369+Ateeth@users.noreply.github.com>
2021-10-05 11:25:45 +0400

    Update test_api.py
    Added comments for 2 or 3 lines

--- Image-Multi-Secret-Sharing-master/test_api.py
+++ Image-Multi-Secret-Sharing-master/test_api.py
@@ -2,18 +2,23 @@ from Encrypt import *
 import cv2
 print ("Please enter the filename of the image to be encrypted")
 file_name = input()
-img = cv2.imread(file_name)
-cv2.imwrite("img1.jpg", img)
+img = cv2.imread(file_name) 
+cv2.imwrite("img1.jpg", img) #overwriting the file name with img1.jpg
 #a, b, c = cv2.split(img)
 print ("The initial shape of the image is {} and the type of the matrix is {}".format(img.shape, type(img)))
 print ("Please enter the value of n, t and k seperated by spaces")
 n, t, k = list(map(int, input().split(" ")))
+
+#whether to show the histogram or not
 print ("Enter 1 to display the histogram else enter 0")
 temp = int(input())
 plot_hist = True if temp == 1 else False
+
+#whether to display the input image or not
 print ("Enter 1 to show the input image else enter 0")
 temp = int(input())
 show_image = True if temp == 1 else False
+
 
 test = Image_Encryption(n=n, t=t, k=k, img=img, show_image=temp, self_debug=True)
 #testb = Image_Encryption(n=n, t=t, k=k, img=b, show_image=temp, self_debug=False)
